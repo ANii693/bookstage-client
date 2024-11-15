792d5e6d0a0f0c4f0933af3295cf0f773111b4b0
anii693 <anirudhrai503@gmail.com>
2024-11-16 00:20:44 +0530

    prof proj commit

---
+++ src/components/home-two/HomeTwoMain.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import HeroSectionTwo from './HeroSectionTwo';
+import FeatureSection from './FeatureSection';
+import BannerSection from './BannerSection';
+import ProductSliderTwo from './ProductSliderTwo';
+import CategorySection from './CategorySection';
+import TrendingProducts from './TrendingProducts';
+import BrandSlider from './BrandSlider';
+import BlogSection from '../home-three/BlogSection';
+
+const HomeTwoMain = () => {
+    return (
+        <>
+            <HeroSectionTwo/>
+            <FeatureSection/>
+            <BannerSection/>
+            <ProductSliderTwo/>
+            <ProductSliderTwo/> {/*2*/}
+            <ProductSliderTwo/> {/*3*/}
+            <CategorySection/>
+            <TrendingProducts/>
+            <BrandSlider/>
+            <BlogSection/>
+        </>
+    );
+};
+
+export default HomeTwoMain;
