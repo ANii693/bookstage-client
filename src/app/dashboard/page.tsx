c7379bc7540d340405fea59eeabee0bcaa46c564
Sunil Kumar P <sunilkpg23@gmail.com>
2024-12-13 16:38:21 +0530

    dashboard added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import BlogGridMain from "@/components/blog-grid/BlogGridMain";
+import Wrapper from "@/layout/DefaultWrapper";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Wrapper>
+        <main>
+          Dashboard
+        </main>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Dashboard;
